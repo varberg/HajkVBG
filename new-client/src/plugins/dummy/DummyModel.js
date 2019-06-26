77fc99e171a3abcc43fa8c8e57d1ef163e2f874f
Jacob Wodzyński <jacob.wod@gmail.com>
2019-06-26 15:16:05 +0200

    Initial push of Cesium integration Not much here, but it loads at least. We will need to find a solution to a webpack build, so that we eventually can have 'npm i cesium' and not load Cesium via a <script> in index.html, as it is right now. Problem is described here: https://stackoverflow.com/questions/56550165/problem-with-cesium-integration-in-openlayers-5-cesium-is-not-defined

--- new-client/src/plugins/dummy/DummyModel.js
+++ new-client/src/plugins/dummy/DummyModel.js
@@ -1,12 +1,27 @@
+import OLCesium from "olcs/OLCesium.js";
+
 class DummyModel {
   constructor(settings) {
     this.map = settings.map;
     this.app = settings.app;
     this.localObserver = settings.localObserver;
+
+    // Setup Cesium
+    this.ol3d = new OLCesium({
+      map: this.map
+    });
   }
 
   getMap() {
     return this.map;
+  }
+
+  toggle3d() {
+    console.log(
+      `Toggeling Cesium ${this.ol3d.getEnabled() === true ? "off" : "on"}.`,
+      this.ol3d
+    );
+    this.ol3d.setEnabled(!this.ol3d.getEnabled());
   }
 }
 
