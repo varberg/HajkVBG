40c74b9cf683ecdd8ad91eb32417a1da8b32f00e
Martin Arvroth Thorbjörnsson <martin.thorbjornsson@sweco.se>
2021-12-29 17:55:17 +0100

    MF-341: Code restructuring

--- new-client/src/plugins/mfintegration/mockdata/mockdataStyle.js
+++ new-client/src/plugins/mfintegration/mockdata/mockdataStyle.js
@@ -39,3 +39,31 @@ export const layerStyle = () => {
     circle: { radius: circleRadius, width: strokeWithCircle },
   };
 };
+
+export const drawNewStyle = () => {
+  const strokeColor = "rgba(255,0,0,0.5)";
+  const strokeWidth = 4;
+  const fillColor = "rgba(255,0,0,0.07)";
+  const circleRadius = 6;
+  const strokeWithCircle = 2;
+
+  return {
+    stroke: { color: strokeColor, width: strokeWidth },
+    fill: { color: fillColor },
+    circle: { radius: circleRadius, width: strokeWithCircle },
+  };
+};
+
+export const newGeometryStyle = () => {
+  const strokeColor = "rgba(255,0,0,0.5)";
+  const strokeWidth = 4;
+  const fillColor = "rgba(255,0,0,0.07)";
+  const circleRadius = 6;
+  const strokeWithCircle = 2;
+
+  return {
+    stroke: { color: strokeColor, width: strokeWidth },
+    fill: { color: fillColor },
+    circle: { radius: circleRadius, width: strokeWithCircle },
+  };
+};
