a70e6c253cbb8d2df5f0221ee93ca1e0c631f7b1
Jacob Wodzyński <jacob.wod@gmail.com>
2022-01-13 14:15:28 +0100

    User control tooltip shows some user options: - Added LocalStorageHelper to User.js to make use of the newly added functionality: LSH will now read and return all contents of the store. - LSH also sends an event, 'localStorageChanged', when setItem is called on LocalStorage. User.js now listens for this event and acts upon change. - User.js will display some key/value pairs of the user's setting. (The selection of settings is not really thought through - see it as a proof of concept, rather.)

--- new-client/src/controls/User.js
+++ new-client/src/controls/User.js
@@ -1,7 +1,9 @@
-import React from "react";
-import { Avatar, Button, Paper, Tooltip } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 
+import { Avatar, Button, Paper, Tooltip } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
+
+import LocalStorageHelper from "utils/LocalStorageHelper";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -46,6 +48,60 @@ const getTooltipString = (userDetails) => {
   return userDetailsArrays.filter((v) => v !== undefined).join(", ");
 };
 
+const TooltipListElement = ({ key, value }, index) => {
+  return (
+    <React.Fragment key={index}>
+      <b>{key}:</b> {value}
+      <br />
+    </React.Fragment>
+  );
+};
+
+const parseStoreDetails = (store, userDetails) => {
+  const elements = [];
+
+  // 'store' will contain all options, not only map-specific.
+  // We must therefore distinguish those two in the first step.
+  // Let's try to grab the map-specific options. If we succeed,
+  // save some of the interesting settings to the elements array.
+  try {
+    const currentMapOptions = JSON.parse(store.store[store.currentMap]);
+
+    elements.push({
+      name: "searchInVisibleLayers",
+      key: "Sök endast i synliga lager",
+      value: currentMapOptions.searchOptions.searchInVisibleLayers
+        ? "Aktiv"
+        : "Inakitv",
+    });
+    elements.push({
+      name: "enableLabelOnHighlight",
+      key: "Visa textetiketter i kartan",
+      value: currentMapOptions.searchOptions.enableLabelOnHighlight
+        ? "Aktiv"
+        : "Inakitv",
+    });
+  } catch (error) {}
+
+  // Now continue grabbing stuff from the store and put into the elements array
+  store.store.userPreferredColorScheme &&
+    elements.push({
+      name: "userPreferredColorScheme",
+      key: "Färgschema",
+      value:
+        store.store.userPreferredColorScheme === "light" ? "Ljust" : "Mörkt",
+    });
+
+  return (
+    <>
+      <p>
+        <b>{getTooltipString(userDetails)}</b>
+      </p>
+      <p>{elements.map(TooltipListElement)}</p>
+    </>
+  );
+};
+
 /**
  * @summary A button that contains user's initials inside an Avatar component
  *
@@ -54,10 +110,23 @@ const getTooltipString = (userDetails) => {
  */
 const User = React.memo(({ userDetails }) => {
   const classes = useStyles();
+  const [store, setStore] = useState(LocalStorageHelper.getReallyAll());
+
+  useEffect(() => {
+    document.addEventListener(
+      "localStorageChanged",
+      localStorageSetHandler,
+      false
+    );
+  }, []);
+
+  const localStorageSetHandler = function () {
+    setStore(LocalStorageHelper.getReallyAll());
+  };
 
   return (
     (userDetails && (
-      <Tooltip title={getTooltipString(userDetails)}>
+      <Tooltip title={parseStoreDetails(store, userDetails)}>
         <Paper className={classes.paper}>
           <Button
             aria-label={userDetails.displayName}
