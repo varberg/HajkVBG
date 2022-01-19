7bc1b3528a7dddc100ed6712ec40496b6a11e69a
Martin Arvroth Thorbjörnsson <martin.thorbjornsson@sweco.se>
2022-01-19 09:04:46 +0100

    Reset the test environment

--- new-client/src/plugins/mfintegration/mockdata/mockdataWFS.js
+++ new-client/src/plugins/mfintegration/mockdata/mockdataWFS.js
@@ -5,7 +5,7 @@ export const wfsConfig = () => {
       geometryField: "fnr_fr",
       geometryName: "geom",
       srsName: "EPSG:3007",
-      url: "http://62.88.129.45:1330/api/v1/proxy/sbk/service/wfs/fastighet/v1",
+      url: "https://geodata.sbk.goteborg.se/service/wfs/fastighet/v1",
     },
     coordinate: {
       featureTypes: ["fastighet.wfs.v1:fastighet"],
